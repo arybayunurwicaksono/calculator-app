46e23f18d2193ae3e1659dab93702193ab1cbc6d
arybayunurwicaksono <ary.bayunurw45@gmail.com>
2020-03-11 14:12:20 +0700

    Inisialisasi Project

---
+++ calculator.cs
@@ -0,0 +1,24 @@
+class Calculator
+{
+	static void Main(String[] args)
+	{
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("Hasil penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.readKey();
+	}
+	
+	static int Penambahan(int a, int b)
+	{
+		return a+b;
+	}
+	
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
+	}
+}
